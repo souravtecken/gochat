1681995032851d9b381330326bf348de671ebf7a
Sourav Raveendran <souravtecken@gmail.com>
2021-06-26 13:11:29 +0530

    Used general json format, shows usernames

--- client/chat/chat.go
+++ client/chat/chat.go
@@ -1,6 +1,7 @@
 package chat
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net"
@@ -15,11 +16,22 @@ func SendMessage(conn *net.Conn, message string) {
 	fmt.Fprintf(*conn, "%s", message)
 }
 
+func decodeMessage(jsonBytes []byte) map[string]interface{} {
+	messageObj := make(map[string]interface{})
+	json.Unmarshal(jsonBytes, &messageObj)
+	return messageObj
+}
+
 func ReceiveMessages(conn *net.Conn, chatDisplay io.Writer) {
 	buf := make([]byte, 2048)
 	for {
 		n, _ := (*conn).Read(buf[0:])
-		message := string(buf[:n])
-		fmt.Fprintf(chatDisplay, "%s\n", message)
+		data := decodeMessage(buf[:n])
+
+		if data["type"] == "message" {
+			// There's probably a cleaner way to do this
+			messageBody := data["data"].(map[string]interface{})
+			fmt.Fprintf(chatDisplay, "%s: %s\n", messageBody["username"].(string), messageBody["message"].(string))
+		}
 	}
 }
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net"
@@ -8,34 +9,49 @@ import (
 
 func encodeData(data interface{}, messageType string) []byte {
 	messageObj := make(map[string]interface{})
-	messageObj["type"] = messageType
 	messageObj["data"] = data
-	jsonString, _ := json.Marshal(messageObj)
-	fmt.Println(jsonString)
-	return []byte(jsonString)
+	messageObj["type"] = messageType
+	jsonBytes, _ := json.Marshal(&messageObj)
+	jsonBytes = bytes.Trim(jsonBytes, "\x00")
+	return jsonBytes
 }
 
-func broadcastMessage(userConMap map[string]net.Conn, message string, username string) {
+func broadcastMessage(userConMap map[string]net.Conn, message []byte) {
 	for _, conn := range userConMap {
-		fmt.Fprintf(conn, message)
+		conn.Write(message)
 	}
+}
+
+func sendMessage(useConMap map[string]net.Conn, message string, username string) {
+	messageBody := make(map[string]interface{})
+	messageBody["message"] = message
+	messageBody["username"] = username
+	data := encodeData(messageBody, "message")
+	fmt.Println(string(data))
+	broadcastMessage(useConMap, data)
 }
 
 func handleConnection(conn net.Conn, userConMap map[string]net.Conn) {
 	fmt.Println("Connection open:", conn.RemoteAddr())
 
 	buf := make([]byte, 512)
-	conn.Read(buf[0:]) // Initial signup message
-	username := string(buf)
+
+	// Initial signup message containing username
+	n, _ := conn.Read(buf[0:])
+	username := string(buf[:n])
+
+	// Add user to connection map
 	userConMap[username] = conn
+
 	fmt.Println(conn.RemoteAddr(), "is", username)
 	for {
 		n, _ := conn.Read(buf[0:])
-		if n == 0 { // Connection has closed
+		if n == 0 {
+			// Connection has closed
 			break
 		}
 		message := string(buf[:n])
-		go broadcastMessage(userConMap, message, username)
+		go sendMessage(userConMap, message, username)
 		fmt.Println(message, n)
 	}
 	delete(userConMap, username)
